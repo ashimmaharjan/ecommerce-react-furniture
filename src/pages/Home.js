6682263b869f3787e39ffa4bbd3f64e5548637fc
ashimmaharjan <ciferulquiorra008@gmail.com>
2022-01-05 20:05:35 +0545

    Home page component created

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <section>
+            <div>
+                <h1 className="text-center text-7xl">Hello Man</h1>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
